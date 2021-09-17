USDepartmentofLabor/ui-claimant-experience-pilot

commit 32181271bb81f807327f8f4e1a2c847433f7d59e

Pull secrets from ENV vars, files (#27)
According to DOL cloud team, k8s integration with AWS secrets manager results in env vars and/or files being available to the container directly, so just assume they are env vars or files with no special handling.

Makefile
@@ -66,8 +66,11 @@ container-run: ## Run the Django app in Docker
 
 container: container-build ## Alias for container-build
 
-# important! this env var must be set to trigger the correct key/config generation.
-build-static: export LOGIN_DOT_GOV_ENV=test
+secret: ## Generate string for SECRET_KEY or REDIS_SECRET_KEY env variable
+	python -c "import secrets; print(secrets.token_urlsafe(32))"
+
+# important! sets the path to the correct .env file to use (e.g. ENV_NAME=ci)
+build-static: export ENV_PATH=/app/core/.env-$(ENV_NAME)
 build-static: ## Build the static assets (intended for during container-build (inside the container))
 	rm -rf static/
 	mkdir static
@@ -115,7 +118,7 @@ react-build: ## Build the React apps
 	for reactapp in $(REACT_APPS); do cd $$reactapp && make build ; done
 
 security: ## Run all security scans
-	bandit -x ./.venv -r . -s B105
+	bandit -x ./.venv -r .
 	safety check
 	for reactapp in $(REACT_APPS); do cd $$reactapp && make security; done
 

core/settings.py
@@ -13,6 +13,7 @@ from pathlib import Path
 import os
 import environ
 import logging
+import logging.config
 from corsheaders.defaults import default_headers
 
 # import pprint
@@ -22,13 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # populate os.environ with .env settings
 env = environ.Env()
-env.read_env()
-
-
-# TODO AWS secrets manager
-def load_secrets():
-    return {}
-
+env.read_env(env.str("ENV_PATH", ".env"))
 
 # since this app usually runs behind one or more reverse proxies that may/not
 # have X-Forwarded-For header set correctly, allow for explicit root URI
@@ -36,19 +31,23 @@ def load_secrets():
 # NOTE this value should *NOT* contain a trailing slash
 BASE_URL = os.environ.get("BASE_URL", None)
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ncqf$a36l&71aeo(^&i0mddiz6-g9zl23)6uvu7@m$$t8%m=s+"
-# TODO load from secrets
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# TODO read from env var
-DEBUG = True
+# env.bool is tricky to get right so opt for strict string comparison
+DEBUG = os.environ.get("DEBUG", "false").lower() == "true"
 
-# TODO read from env var
+# tie together multiple log lines with a common id
+REQUEST_ID_CONFIG = {
+    "REQUEST_ID_HEADER": "HTTP_X_REQUEST_ID",
+    "GENERATE_REQUEST_ID_IF_NOT_FOUND": True,
+    "RESPONSE_HEADER_REQUEST_ID": "HTTP_X_REQUEST_ID",
+}
+
 ALLOWED_HOSTS = [
     "localhost",
     ".dol.gov",
@@ -56,12 +55,51 @@ ALLOWED_HOSTS = [
     ".ui.gov",
 ]
 
-# TODO colorized logging?
 logging.basicConfig(
     level=logging.DEBUG if DEBUG else logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 logger = logging.getLogger("core")
+if os.environ.get("COLOR_LOGGING", "false").lower() == "true":
+    LOGGING_CONFIG = None  # This empties out Django's logging config
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "filters": {
+            "request_id": {"()": "request_id_django_log.filters.RequestIDFilter"}
+        },
+        "formatters": {
+            "colored": {
+                "()": "colorlog.ColoredFormatter",  # colored output
+                # --> %(log_color)s is very important, that's what colors the line
+                "format": "%(log_color)s[%(levelname)s] %(reset)s %(green)s[%(request_id)s] %(reset)s%(blue)s%(name)s - %(asctime)s :: %(reset)s %(message)s",
+                "log_colors": {
+                    "DEBUG": "blue",
+                    "INFO": "green",
+                    "WARNING": "yellow",
+                    "ERROR": "red",
+                    "CRITICAL": "bold_red",
+                },
+            },
+            # TODO determine config for CloudWatch
+            "aws": {
+                "format": "%(asctime)s - %(name)s - %(levelname)s - %(funcName)s:%(lineno)d - %(message)s",
+                "datefmt": "%Y-%m-%d %H:%M:%S",
+            },
+        },
+        "handlers": {
+            "console": {
+                "level": "DEBUG",
+                "class": "colorlog.StreamHandler",
+                "formatter": "colored",
+                "filters": ["request_id"],
+            },
+        },
+        "loggers": {
+            "": {"handlers": ["console"], "level": "DEBUG", "propagate": False},
+        },
+    }
+    logging.config.dictConfig(LOGGING)  # Finally replace our config in python logging
 
 # Application definition
 
@@ -84,6 +122,7 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "request_id_django_log.middleware.RequestIdDjangoLog",
 ]
 
 ROOT_URLCONF = "core.urls"
@@ -114,11 +153,11 @@ CACHES = {
         | {
             # 'PARSER_CLASS': 'redis.connection.HiredisParser',
             # A URL-safe base64-encoded 32-byte key.
-            "REDIS_SECRET_KEY": "kPEDO_pSrPh3qGJVfGAflLZXKAh4AuHU64tTlP-f_PY=",  # TODO load from secrets
+            "REDIS_SECRET_KEY": os.environ.get("REDIS_SECRET_KEY"),
             "SERIALIZER": "secure_redis.serializer.SecureSerializer",
         },
         "KEY_PREFIX": "claimantsapi:secure",
-        "TIMEOUT": 60 * 15,  # expire in 15 minutes TODO security requirement
+        "TIMEOUT": 60 * 30,  # expire in 30 minutes TODO security requirement
     },
     "insecure": {
         "BACKEND": "django_redis.cache.RedisCache",
@@ -139,17 +178,18 @@ SESSION_EXPIRY = env.int(
     "SESSION_EXPIRY", 30 * 60
 )  # 30 minute timeout on no requests TODO
 SESSION_COOKIE_AGE = env.int("SESSION_EXPIRY", 30 * 60)
-# allow XHR/CORS to work in local dev with http/https mix
-# CSRF_COOKIE_SAMESITE = 'None'
+# allow XHR/CORS to work in local dev with http/https mix,
+# SESSION_COOKIE_SAMESITE is set to None in the .env-example for dev.
 # NOTE that this assumes you are running react app on http and django on https behind proxy
 # Chrome requires SameSite=None to be paired with Secure
-SESSION_COOKIE_SAMESITE = "None"
-SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SAMESITE = os.environ.get("SESSION_COOKIE_SAMESITE", "Strict")
+SESSION_COOKIE_SECURE = (
+    os.environ.get("SESSION_COOKIE_SECURE", "true").lower() == "true"
+)
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-mysql_default = "mysql://user:secret@host.docker.internal:3306/unemployment"
-default_db = env.db_url("DATABASE_URL", mysql_default)
+default_db = env.db_url("DATABASE_URL")
 # allow for password to be stored separately from connection string
 if not default_db["PASSWORD"]:
     default_db["PASSWORD"] = env("DATABASE_PASSWORD")
@@ -207,17 +247,11 @@ TEMPLATES = [
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOWED_ORIGINS = [
-    "https://sandbox.ui.dol.gov:4430",
-    "http://sandbox.ui.dol.gov:8004",
-    "http://sandbox.ui.dol.gov:3000",
-    "http://localhost:8004",
-]
-CORS_ALLOWED_ORIGIN_REGEXES = [
-    r"^https://.+\.dol\.gov$",
-    r"^https://.+\.unemployment\.gov$",
-    r"^https://.+\.ui\.gov$",
-]
+# CORS Cross-Origin Configuration
+# https://github.com/adamchainz/django-cors-headers
+CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=[])
+CORS_ALLOWED_ORIGIN_REGEXES = env.list("CORS_ALLOWED_ORIGIN_REGEXES", default=[])
+# important to allow cookie to pass through
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = list(default_headers) + [
     "Cache-Control",
@@ -228,24 +262,16 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 ]
 
 # Identity Providers
-LOGIN_DOT_GOV_REDIRECT_URI = os.environ.get(
-    "LOGIN_DOT_GOV_REDIRECT_URI", "https://sandbox.ui.dol.gov:4430/logindotgov/result"
-)
-LOGIN_DOT_GOV_SCOPES = env.list(
-    "LOGIN_DOT_GOV_SCOPES",
-    default=[
-        "openid",
-        "email",
-        "phone",
-        "address",
-        "profile",
-        "social_security_number",
-    ],
-)
-LOGIN_DOT_GOV_CLIENT_ID = os.environ.get(
-    "LOGIN_DOT_GOV_CLIENT_ID",
-    "urn:gov:gsa:openidconnect.profiles:sp:sso:dol:ui-arpa-claimant-sandbox",
-)
+LOGIN_DOT_GOV_REDIRECT_URI = os.environ.get("LOGIN_DOT_GOV_REDIRECT_URI")
+LOGIN_DOT_GOV_SCOPES = [
+    "openid",
+    "email",
+    "phone",
+    "address",
+    "profile",
+    "social_security_number",
+]
+LOGIN_DOT_GOV_CLIENT_ID = os.environ.get("LOGIN_DOT_GOV_CLIENT_ID")
 
 if os.environ.get("LOGIN_DOT_GOV_ENV") == "test":
     # generate a new key pair on the fly
@@ -263,7 +289,6 @@ if os.environ.get("LOGIN_DOT_GOV_ENV") == "test":
     )
     LOGIN_DOT_GOV_PUBLIC_KEY = client_public_key_jwk.export_to_pem().decode("utf-8")
 else:  # pragma: no cover
-    # TODO read from secrets
     logindotgov_private_key = ""
     private_key_file = (
         BASE_DIR

.github/workflows/pull_request.yml
@@ -19,6 +19,7 @@ jobs:
       run: make ci-start
       env:
         DOCKER_BUILDKIT: 1
+        ENV_NAME: ci
     - name: Run Django Tests
       run: make ci-tests
     # we must build the react apps (again) in the native host in order to run the tests

Dockerfile
@@ -21,6 +21,8 @@ RUN make react-build
 
 FROM python:3.9
 WORKDIR /app
+ARG ENV_NAME
+ENV ENV_NAME $ENV_NAME
 
 COPY requirements.txt .
 

core/.env-ci
@@ -1,5 +1,6 @@
 # CI env
 #
+ENV_NAME=ci
 DATABASE_URL=mysql://user:@rds:3306/unemployment
 DATABASE_PASSWORD=secret
 BASE_URL=https://sandbox.ui.dol.gov:4430
@@ -7,6 +8,11 @@ BASE_URL=https://sandbox.ui.dol.gov:4430
 REDIS_URL=rediss://elasticache:6379/0
 
 LOGIN_DOT_GOV_REDIRECT_URI=https://sandbox.ui.dol.gov:4430/logindotgov/result
-LOGIN_DOT_GOV_SCOPES=openid,email,phone,address,profile,social_security_number
 LOGIN_DOT_GOV_CLIENT_ID=urn:gov:gsa:openidconnect.profiles:sp:sso:dol:ui-arpa-claimant-sandbox
 LOGIN_DOT_GOV_ENV=test
+
+SECRET_KEY=QTljBX1CZvgT1p7EhTmk6aX7gWeSXQM1_M4j_gdPsoQ
+REDIS_SECRET_KEY=NmJjZjBjMmM3NTdiZTM3MDFkMGQ2YjFjOTMzM2Y0ZDY=
+
+SESSION_COOKIE_SAMESITE=None
+SESSION_COOKIE_SECURE=True

core/.env-example
@@ -5,6 +5,12 @@ DATABASE_URL=mysql://user:@host.docker.internal:3306/unemployment
 DATABASE_PASSWORD=secret
 BASE_URL=https://sandbox.ui.dol.gov:4430
 
+# DEBUG on in dev
+DEBUG=true
+
+# set this for local dev to ease your eyes reading Django logs
+COLOR_LOGGING=true
+
 # login.gov sandbox for local dev only
 LOGIN_DOT_GOV_REDIRECT_URI=https://sandbox.ui.dol.gov:4430/logindotgov/result
 LOGIN_DOT_GOV_SCOPES=openid,email,phone,address,profile,social_security_number
@@ -17,3 +23,13 @@ LOGIN_DOT_GOV_ENV=sandbox
 # REDIS_HOST=host.docker.internal
 # REDIS_PORT=6379
 # NOTE that docker compose service listens on port 6379 with TLS enabled.
+
+SECRET_KEY=django-insecure-ncqf$a36l&71aeo(^&i0mddiz6-g9zl23)6uvu7@m$$t8%m=s+
+SESSION_COOKIE_SAMESITE=None
+SESSION_COOKIE_SECURE=True
+CORS_ALLOWED_ORIGINS=https://sandbox.ui.dol.gov:4430,http://sandbox.ui.dol.gov:8004,http://sandbox.ui.dol.gov:3000,http://localhost:8004
+CORS_ALLOWED_ORIGIN_REGEXES=https://.+\.dol\.gov,https://.+\.unemployment\.gov,https://.+\.ui\.gov
+
+# NOTE this must be a URL-safe base64-encoded 32-byte key
+# It is used for encryption of Redis values.
+REDIS_SECRET_KEY=kPEDO_pSrPh3qGJVfGAflLZXKAh4AuHU64tTlP-f_PY=

core/views.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 import logging
+from home.views import base_url
 
 logger = logging.getLogger("core")
 
 
 def initclaim(request):
-    return render(None, "build/index.html")
+    return render(None, "build/index.html", {"base_url": base_url(request)})

docker-compose-ci.yml
@@ -11,6 +11,8 @@ services:
     image: dolui:ci
     build:
       context: .
+      args:
+        ENV_NAME: ci
     ports:
       - "8000:8000"
     # set env at run time, but also populate the build args values

commit 9cd443409b709999c185c61d33ae66b4b52287c1

Only set ENV_PATH when ENV_NAME is set (#38)
Regression introduced in #27 affecting local docker build.

Makefile
@@ -70,8 +70,14 @@ secret: ## Generate string for SECRET_KEY or REDIS_SECRET_KEY env variable
 	python -c "import secrets; print(secrets.token_urlsafe(32))"
 
 # important! sets the path to the correct .env file to use (e.g. ENV_NAME=ci)
-build-static: export ENV_PATH=/app/core/.env-$(ENV_NAME)
+ifeq ($(ENV_NAME),)
+  ENV_FILENAME = .env
+else
+  ENV_FILENAME = .env-$(ENV_NAME)
+endif
+build-static: export ENV_PATH=/app/core/$(ENV_FILENAME)
 build-static: ## Build the static assets (intended for during container-build (inside the container))
+	echo $$ENV_PATH
 	rm -rf static/
 	mkdir static
 	python manage.py collectstatic

core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # populate os.environ with .env settings
 env = environ.Env()
-env.read_env(env.str("ENV_PATH", ".env"))
+env.read_env(env.str("ENV_PATH", "core/.env"))
 
 # since this app usually runs behind one or more reverse proxies that may/not
 # have X-Forwarded-For header set correctly, allow for explicit root URI