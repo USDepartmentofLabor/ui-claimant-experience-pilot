ad9983662ed9f144ae7ec95484a3ae7c3c450d32
Peter Karman <pkarman@users.noreply.github.com>
2021-09-30 08:56:57 -0500

    Add cypress tests, /login page (#73)

---
+++ claimant/cypress/support/commands.js
@@ -0,0 +1,45 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import "cypress-audit/commands";
+
+/* eslint-disable no-undef */
+
+// test login uses local form page
+Cypress.Commands.add(
+  "login",
+  () => {
+    cy.visit("/login/");
+    cy.get("#email").should("be.visible").type("someone@example.com");
+    cy.get('button[type="submit"]')
+      .should('be.visible')
+      .click();
+  }
+);
+
+Cypress.Commands.overwrite("log", (subject, message) =>
+  cy.task("log", message)
+);
--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
-from django.http import JsonResponse
 from core.utils import session_as_dict
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 import logging
 
 logger = logging.getLogger("home")
@@ -24,6 +25,29 @@ def test(request):  # pragma: no cover
     this_session = session_as_dict(request)
     this_session["test_cookie_worked"] = request.session.test_cookie_worked()
     return JsonResponse(this_session)
+
+
+# NOTE this login page is for testing only, it is not routed when DEBUG is false
+# see views.py
+@csrf_exempt
+def login(request):
+    if request.method == "GET":
+        request.session.set_test_cookie()
+        return render(None, "login.html", {"base_url": base_url(request)})
+    elif request.method == "POST":
+        request.session["verified"] = True
+        whoami = {}
+        for k in request.POST.keys():
+            whoami[k] = request.POST[k]
+        request.session["whoami"] = whoami
+        redirect_to = "/claimant/"
+        if "redirect_to" in request.session:
+            redirect_to = request.session["redirect_to"]
+            del request.session["redirect_to"]
+        logger.debug("redirect_to={}".format(redirect_to))
+        return redirect(redirect_to)
+    else:
+        return HttpResponse("GET or POST", 405)
 
 
 def base_url(request):  # pragma: no cover
