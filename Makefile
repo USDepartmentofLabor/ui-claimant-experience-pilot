USDepartmentofLabor/ui-claimant-experience-pilot

commit c08ad0bb8caac01dba50a9930be9b219519bc8d8

Add test coverage, report verbosity (#239)
* Add test coverage, report verbosity

* rename vars for clarity

* allow make container to setup for cypress tests

Makefile
@@ -132,7 +132,7 @@ container-rm: ## Remove the Django app container with DOCKER_CONTAINER_ID
 container-clean: ## Remove the Django app container image
 	docker image rm $(DOCKER_IMG)
 
-container: container-clean container-build ## Alias for container-clean container-build
+container: container-clean container-build ci-setup-react-tests ## Alias for container-clean container-build ci-setup-react-tests
 
 SECRET_LENGTH := 32
 secret: ## Generate string for SECRET_KEY or REDIS_SECRET_KEY env variable

Dockerfile
@@ -75,6 +75,7 @@ CMD ["./start-server.sh"]
 # for local development
 
 FROM djangobase as djangobase-devlocal
+COPY setup-cypress-tests.sh .
 RUN if [ -f core/.env ] ; then echo "core/.env exists" ; else cp core/.env-example core/.env ; fi && \
   pip install --no-cache-dir -r requirements-ci.txt
 

claimant/cypress/integration/tests/claim-form.spec.js
@@ -1,6 +1,11 @@
 /* eslint-disable no-undef */
 
 context("Initial Claim form", { scrollBehavior: false }, () => {
+  it("requires login", () => {
+    cy.visit("/claimant/");
+    cy.url().should("include", "/idp/?redirect_to=");
+  });
+
   it("sends test email", () => {
     cy.login();
     cy.visit("/claimant/");

claimant/src/coverageCheck.js
@@ -3,10 +3,8 @@
 const fs = require("fs");
 const path = require("path");
 
-// TODO these should be moved up to 90 and 50, respectively,
-// as coverage increases. They are set low to start with,
-// so that we can integrate the test coverage infra.
-const MINIMUM_GLOBAL = 75;
+const MINIMUM_STATEMENTS = 90;
+const MINIMUM_BRANCHES = 75;
 const MINIMUM_INDIVIDUAL = 50;
 
 let fileData;
@@ -26,10 +24,10 @@ try {
 
 const coverageShortfalls = {};
 try {
-  if (fileData.total.statements.pct < MINIMUM_GLOBAL) {
+  if (fileData.total.statements.pct < MINIMUM_STATEMENTS) {
     coverageShortfalls.totalStatements = fileData.total.statements.pct;
   }
-  if (fileData.total.branches.pct < MINIMUM_GLOBAL) {
+  if (fileData.total.branches.pct < MINIMUM_BRANCHES) {
     coverageShortfalls.totalBranches = fileData.total.branches.pct;
   }
   Object.keys(fileData).forEach((key) => {
@@ -38,9 +36,15 @@ try {
     }
     if (fileData[key].statements.pct < MINIMUM_INDIVIDUAL) {
       coverageShortfalls[`${key}_statement`] = fileData[key].statements.pct;
+      console.log(
+        `File statements coverage for ${key} is ${fileData[key].statements.pct}`
+      );
     }
     if (fileData[key].branches.pct < MINIMUM_INDIVIDUAL) {
       coverageShortfalls[`${key}_branch`] = fileData[key].branches.pct;
+      console.log(
+        `File branches coverage for ${key} is ${fileData[key].branches.pct}`
+      );
     }
   });
 } catch (error) {
@@ -48,16 +52,19 @@ try {
   process.exit(1);
 }
 
+console.log(`Total statement coverage is ${fileData.total.statements.pct}%`);
+console.log(`Total branch coverage is ${fileData.total.branches.pct}%`);
+
 if (Object.keys(coverageShortfalls).length) {
   console.error("Test coverage is below required levels.");
   if (coverageShortfalls.totalStatements) {
     console.log(
-      `Total statement coverage is ${coverageShortfalls.totalStatements}% but should be at least ${MINIMUM_GLOBAL}%`
+      `Total statement coverage is ${coverageShortfalls.totalStatements}% but should be at least ${MINIMUM_STATEMENTS}%`
     );
   }
   if (coverageShortfalls.totalBranches) {
     console.log(
-      `Total branch coverage is ${coverageShortfalls.totalBranches}% but should be at least ${MINIMUM_GLOBAL}%`
+      `Total branch coverage is ${coverageShortfalls.totalBranches}% but should be at least ${MINIMUM_BRANCHES}%`
     );
   }
   Object.keys(coverageShortfalls).forEach((key) => {

claimant/src/queries/claim.test.tsx
@@ -1,34 +1,0 @@
-import { AxiosInstance } from "axios";
-import { renderHook } from "@testing-library/react-hooks";
-import { QueryClientProvider, QueryClient } from "react-query";
-import httpclient from "../utils/httpclient";
-import { useSubmitClaim } from "./claim";
-
-jest.mock("../utils/httpclient");
-const mockedHttpClient = httpclient as jest.Mocked<AxiosInstance>;
-
-describe("submit claim", () => {
-  const createWrapper = () => {
-    const queryClient = new QueryClient();
-    const wrapper = ({ children }: any) => (
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    );
-    return wrapper;
-  };
-
-  // TODO: bail on this test in favor of using a service worker on the app page to cover it
-  it.skip("calls the api and returns the payload", async () => {
-    const claimResponse = { status: "submitted", claim_id: "123" };
-
-    mockedHttpClient.post.mockResolvedValueOnce({ data: claimResponse });
-    const { result, waitFor } = renderHook(() => useSubmitClaim(), {
-      wrapper: createWrapper(),
-    });
-
-    await waitFor(() => result.current.isSuccess);
-
-    expect(mockedHttpClient.post).toHaveBeenCalledTimes(1);
-    expect(mockedHttpClient.post).toHaveBeenCalledWith("/api/claim");
-    expect(result.current.status).toEqual("success");
-  });
-});

commit cc5221a215efad506623a754cc4d16980ea5f6fd

Fix container build setup (#245)
Fixes regression introduced in #239 

For local development with cypress tests, you must set up your environment. You can do that like:

```sh
% make container container-run
# in a separate window
% make container-setup-react-tests
```

NOTE that you only need to run the setup once as long as your local DB persists.

Makefile
@@ -132,7 +132,10 @@ container-rm: ## Remove the Django app container with DOCKER_CONTAINER_ID
 container-clean: ## Remove the Django app container image
 	docker image rm $(DOCKER_IMG)
 
-container: container-clean container-build ci-setup-react-tests ## Alias for container-clean container-build ci-setup-react-tests
+container-setup-react-tests: ## Create local artifacts required for running Cypress tests
+	docker exec -it $(DOCKER_CONTAINER_ID) ./setup-cypress-tests.sh
+
+container: container-clean container-build ## Alias for container-clean container-build
 
 SECRET_LENGTH := 32
 secret: ## Generate string for SECRET_KEY or REDIS_SECRET_KEY env variable