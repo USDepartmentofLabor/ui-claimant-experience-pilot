f74e569160ccdf52624a0d563082f826f3b54a01
Peter Karman <pkarman@users.noreply.github.com>
2021-11-17 13:55:13 -0600

    Add REACT_APP_FAKE_WHOAMI env var to fake authentication (#254)
    * Add REACT_APP_FAKE_WHOAMI env var to fake authentication
    
    * document FAKE_WHOAMI
    
    * leave commented out
    
    * do not allow FAKE_WHOAMI in production

--- claimant/src/queries/whoami.ts
+++ claimant/src/queries/whoami.ts
@@ -3,6 +3,12 @@ import { useQuery } from "react-query";
 import httpclient from "../utils/httpclient";
 
 const getWhoAmI = async () => {
+  if (
+    process.env.NODE_ENV !== "production" &&
+    process.env.REACT_APP_FAKE_WHOAMI
+  ) {
+    return JSON.parse(process.env.REACT_APP_FAKE_WHOAMI);
+  }
   const { data } = await httpclient.get<WhoAmI>("/api/whoami/", {
     withCredentials: true,
     headers: { "X-DOL": "axios" },
