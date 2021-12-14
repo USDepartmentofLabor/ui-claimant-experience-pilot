f91dfdfbebe008957159ba273bbfa4ed9249f339
Isaac Garfinkle <isaac@truss.works>
2021-12-13 17:19:09 -0800

    Use new address component

--- claimant/src/pages/Questions/ContactInformation.tsx
+++ claimant/src/pages/Questions/ContactInformation.tsx
@@ -4,6 +4,7 @@ import { ClaimSchemaFields } from "../../common/YupBuilder";
 import DropdownField from "../../components/form/fields/DropdownField";
 import FieldGroup from "../../components/form/FieldGroup";
 import CheckboxField from "../../components/form/fields/CheckboxField";
+import Address from "../../components/form/Address/Address";
 
 // the schema fields that appear on this page
 // only worry about top-level objects
@@ -20,42 +21,14 @@ export const ContactInformation = () => {
 
   return (
     <>
-      <FieldGroup>
-        <TextField
-          name="address.address1"
-          label={t("label.address.address1")}
-          type="text"
-          id="address.address1"
-        />
-        <TextField
-          name="address.address2"
-          label={t("label.address.address2")}
-          type="text"
-          id="address.address2"
-        />
-        <TextField
-          name="address.city"
-          label={t("label.address.city")}
-          type="text"
-          id="address.city"
-        />
-        <DropdownField
-          name="address.state"
-          label={t("label.address.state")}
-          id="address.state"
-          options={[
-            //placeholder
-            { id: "AR", label: "Arkansas" },
-            { id: "NJ", label: "New Jersey" },
-          ]}
-        />
-        <TextField
-          name="address.zip"
-          label={t("label.address.zip")}
-          type="text"
-          id="address.zip"
-        />
-      </FieldGroup>
+      <Address
+        basename="residence_address"
+        states={[
+          //placeholder
+          { id: "AR", label: "Arkansas" },
+          { id: "NJ", label: "New Jersey" },
+        ]}
+      />
     </>
   );
 };
