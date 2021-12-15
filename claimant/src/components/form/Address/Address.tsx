7f950ee1f8d885849b52d55437faa7f26e75fbbc
Peter Karman <pkarman@users.noreply.github.com>
2021-12-15 09:43:55 -0600

    Dereference $ref in JSON schema (#352)
    # Dereference `$ref` in JSON schema
    
    This [bug in the Python jsonschema](https://github.com/Julian/jsonschema/issues/382#issuecomment-993121038) library was forcing us to write our schema in a way that prevented YupBuilder to correctly de-reference our nested definitions.
    
    Now our schema is correct, and both our JS and Python tools completely de-reference the raw schema before attempting to use it, just to be absolutely sure we are using `$ref` correctly.
    
    ## Changes proposed in this pull request
    
    - Fix `$ref` links in schema to always use `#/definitions/` syntax
    - Use `jsonschema` directly in our `pre-commit` hook
    - Deference all `$ref` pointers before using the schema in both JS and Python
    - update `make dev-deps` for required tools

--- claimant/src/components/form/Address/Address.tsx
+++ claimant/src/components/form/Address/Address.tsx
@@ -1,7 +1,6 @@
 import { useTranslation } from "react-i18next";
 import TextField from "../fields/TextField";
 import DropdownField from "../fields/DropdownField/DropdownField";
-// import FieldGroup from "../FieldGroup/FieldGroup";
 
 type StateType = {
   id: string;
