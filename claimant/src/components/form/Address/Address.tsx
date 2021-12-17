820184b1764bd157350df9a1a0ee529da681b0ab
Peter Karman <pkarman@users.noreply.github.com>
2021-12-17 10:19:59 -0600

    Address with custom labels (#375)
    * Address takes explicit labels
    
    * refactor common address labels
    
    * tests
    
    * lint
    
    * review feedback

--- claimant/src/components/form/Address/Address.tsx
+++ claimant/src/components/form/Address/Address.tsx
@@ -6,7 +6,16 @@ import { FormGroup } from "@trussworks/react-uswds";
 
 type StateAbbrev = keyof typeof states;
 
+interface IAddressLabels {
+  address1: string;
+  address2: string;
+  city: string;
+  state: string;
+  zipcode: string;
+}
+
 interface IAddressProps {
+  labels?: IAddressLabels;
   basename: string;
   stateSlice?: StateAbbrev[];
 }
@@ -20,11 +29,15 @@ const stateOptions: StateType[] = Object.keys(statesByAbbrev).map((abbrev) => {
   return { value: abbrev, label: statesByAbbrev[abbrev] };
 });
 
-export const Address = ({
-  basename,
-  stateSlice = undefined,
-}: IAddressProps) => {
-  const { t } = useTranslation("common"); // TODO make this a prop name
+export const Address = ({ labels, basename, stateSlice }: IAddressProps) => {
+  const { t } = useTranslation("common");
+  const defaultLabels: IAddressLabels = {
+    address1: t("address.address1.label"),
+    address2: t("address.address2.label"),
+    city: t("address.city.label"),
+    state: t("address.state.label"),
+    zipcode: t("address.zipcode.label"),
+  };
 
   let stateDropdownOptions: StateType[] = stateOptions;
   if (stateSlice) {
@@ -32,38 +45,38 @@ export const Address = ({
       (opt) => !stateSlice.includes(opt.value as StateAbbrev)
     );
   }
-  stateDropdownOptions.unshift({ value: "", label: t("label.select_one") });
+  stateDropdownOptions.unshift({ value: "", label: t("select_one") });
 
   return (
     <FormGroup>
       <TextField
         name={`${basename}.address1`}
-        label={t("label.address1")}
+        label={labels ? labels.address1 : defaultLabels.address1}
         type="text"
         id={`${basename}.address1`}
       />
       <TextField
         name={`${basename}.address2`}
-        label={t("label.address2")}
+        label={labels ? labels.address2 : defaultLabels.address2}
         type="text"
         id={`${basename}.address2`}
       />
       <TextField
         name={`${basename}.city`}
-        label={t("label.city")}
+        label={labels ? labels.city : defaultLabels.city}
         type="text"
         id={`${basename}.city`}
       />
       <DropdownField
         name={`${basename}.state`}
-        label={t("label.state")}
+        label={labels ? labels.state : defaultLabels.state}
         id={`${basename}.state`}
         options={stateDropdownOptions}
       />
       <TextField
         // TODO pass medium
         name={`${basename}.zipcode`}
-        label={t("label.zipcode")}
+        label={labels ? labels.zipcode : defaultLabels.zipcode}
         type="text"
         id={`${basename}.zipcode`}
         // TODO pattern="[\d]{5}(-[\d]{4})?"
