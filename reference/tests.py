4d768e1feece9c1040bc67dfd9b991e80ac0860c
Nick Scialli <49658917+nickscialli-usds@users.noreply.github.com>
2022-03-17 14:42:06 -0400

    Small template tweaks in reference site (#893)
    * Add placeholder content to intro page
    
    * Update template and add second page

--- reference/tests.py
+++ reference/tests.py
@@ -15,3 +15,15 @@ class ReferenceTestCase(TestCase):
         patched_ld_client.variation.return_value = True
         response = self.client.get("/reference/")
         self.assertEqual(response.status_code, 200)
+
+    @patch("reference.middleware.visible.ld_client")
+    def test_plain_language_page_hidden(self, patched_ld_client):
+        patched_ld_client.variation.return_value = False
+        response = self.client.get("/reference/plain-language/")
+        self.assertEqual(response.status_code, 404)
+
+    @patch("reference.middleware.visible.ld_client")
+    def test_plain_language_page_shown(self, patched_ld_client):
+        patched_ld_client.variation.return_value = True
+        response = self.client.get("/reference/plain-language/")
+        self.assertEqual(response.status_code, 200)
--- reference/urls.py
+++ reference/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("plain-language/", views.plain_language, name="plain_language"),
 ]
