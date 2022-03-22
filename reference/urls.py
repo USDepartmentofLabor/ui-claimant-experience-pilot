2930bb8f602f25f50e64ef3463d1227e196ea4ad
Nick Scialli <49658917+nickscialli-usds@users.noreply.github.com>
2022-03-22 13:00:36 -0400

    Reference site content and formatting (#910)
    * Reference site content and formatting
    
    * Add ref site side menu
    
    * Add tests, small wording change

--- reference/urls.py
+++ reference/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("plain-language/", views.plain_language, name="plain_language"),
+    path("open-source/", views.open_source, name="open_source"),
 ]
